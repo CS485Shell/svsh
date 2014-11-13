d1f27b71e9f8ac3dbd56c87b2b32f9d00500dc8c
bhushant3769 <bhushan.chitre@hotmail.com>
2014-11-13 15:28:37 -0500

    Create svsh.h

---
+++ svsh.h
@@ -0,0 +1,14 @@
+/* 
+ * File:   svsh.h
+ * Author: Bhushan
+ *
+ * Created on November 13, 2014, 1:57 PM
+ */
+
+#ifndef SVSH_H
+#define	SVSH_H
+#include <stdlib.h>
+
+
+void userCmd(ARGL *argl, char *input, char *result);
+
